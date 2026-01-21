cb3ae72a13f20c14060b3ad2f79ffb12a443f91f
PC5 <diegosf9009@gmail.com>
2026-01-20 20:57:20 -0600

    Avance

---
+++ Assets/Scripts/ButtonsManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections;
+public class ButtonsManager : MonoBehaviour
+{
+    
+[SerializeField]
+
+private Animator [] buttons;
+[SerializeField]
+
+private string showAnimationName = "Show";
+[SerializeField]
+
+private string hideAnimationName = "Hide";
+[SerializeField]
+
+private Animator closeButton;
+
+
+public void ShowCloseButton()
+    {
+        closeButton.Play(showAnimationName, 0, 0f);
+    }
+
+
+public void HideCloseButton()
+    {
+        
+        closeButton.Play(hideAnimationName, 0, 0f);
+
+    }
+
+public void ShowButtons(float delay = 0f)
+    {
+        float delayButton = 0;
+        foreach (Animator button in buttons)
+        {
+            StartCoroutine(PlayWithDelay(button, showAnimationName, delay));
+            delayButton+= delay;
+        }
+    }
+
+    public void HideButtons(float delay = 0f)
+    {
+        float delayButton = 0;
+        foreach(Animator button in buttons)
+        {
+            StartCoroutine(PlayWithDelay(button, hideAnimationName, delay));
+            delayButton+= delay;
+        }
+
+    }
+
+    private IEnumerator PlayWithDelay(Animator button, string animationName, float delay)
+    {
+        
+        yield return new WaitForSeconds(delay);
+        button.Play(animationName, 0, 0f);
+    }
+
+
+
+
+}
